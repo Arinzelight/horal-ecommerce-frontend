c91a922a2d3c5ff9641e1e3a37abe48374a67408
Arinzelight <arinzelight2@gmail.com>
2025-05-14 16:16:52 +0100

    update order details stepper and move buttons down the page

--- src/pages/order-details/OrderStepper.jsx
+++ src/pages/order-details/OrderStepper.jsx
@@ -29,13 +29,22 @@ const OrderStepper = () => {
       {/* Stepper wrapper */}
       <div className="flex flex-row w-full justify-between items-center gap-0 relative">
         {/* Connector line for md+ */}
-        <div className="block absolute sm:top-[10px] top-[4px] md:left-[5%]  sm:left-[5.5%] left-[12%] sm:right-[3%] right-[12%] md:right-[3.8%] h-1 bg-gray-300 z-0" />
+        <div className="block absolute sm:top-[35px] top-[25px] md:left-[5%]  sm:left-[5.5%] left-[12%] sm:right-[3%] right-[12%] md:right-[3.8%] h-1 bg-gray-300 z-0" />
 
         {steps.map((step, index) => (
           <div
             key={index}
-            className="flex flex-col items-center gap-1 z-10 text-center w-full md:w-auto"
+            className="flex flex-col items-center gap-2 z-10 text-center w-full md:w-auto"
           >
+            {/* Label */}
+            <div
+              className={`text-[8px] md:text-sm font-medium ${
+                step.completed ? "text-green-500" : "text-gray-400"
+              }`}
+            >
+              {step.label}
+            </div>
+
             {/* Dot */}
             <div
               className={`w-3 h-3 md:w-5 md:h-5 rounded-full ${
@@ -43,17 +52,8 @@ const OrderStepper = () => {
               }`}
             ></div>
 
-            {/* Label */}
-            <div
-              className={`text-[7px] md:text-sm font-medium ${
-                step.completed ? "text-green-500" : "text-gray-400"
-              }`}
-            >
-              {step.label}
-            </div>
-
             {/* Date */}
-            <div className="text-[6px] md:text-xs text-neutral-500">
+            <div className="text-[8px] md:text-xs text-neutral-500">
               {step.date}
             </div>
           </div>
