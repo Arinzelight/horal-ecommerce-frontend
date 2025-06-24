0535e7a169ea77dd858572dcd909d6d788038b87
Arinzelight <arinzelight2@gmail.com>
2025-06-24 11:35:34 +0100

    share products to various sections

--- src/pages/home/HotProductSection.jsx
+++ src/pages/home/HotProductSection.jsx
@@ -2,13 +2,8 @@ import { FaChevronRight } from "react-icons/fa";
 import ProductCard from "../../components/ProductCard";
 import useMobile from "../../hooks/use-mobile";
 
-const HotProductSection = ({ products = {} }) => {
+const HotProductSection = ({ topProducts }) => {
   const isMobile = useMobile();
-
-  console.log("HotProductSection products:", products);
-
-  // Extract the results array from the products object
-  const productsList = products.results || [];
 
   return (
     <div className="pb-4 pt-4">
@@ -23,14 +18,14 @@ const HotProductSection = ({ products = {} }) => {
       </div>
 
       {/* Product Grid */}
-      {productsList.length === 0 && (
+      {topProducts.length === 0 && (
         <div className="col-span-2 md:col-span-4 text-center">
           <p className="text-gray-500">No products found.</p>
         </div>
       )}
 
       <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
-        {productsList.map((product) => (
+        {topProducts.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
       </div>
