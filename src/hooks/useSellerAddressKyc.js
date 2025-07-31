da85a141922812f8d39e829a6d1a61ee40084a22
Arinzelight <arinzelight2@gmail.com>
2025-07-31 11:42:27 +0100

    fix api import error

--- src/hooks/useSellerAddressKyc.js
+++ src/hooks/useSellerAddressKyc.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import api from "@/services/api";
+import api from "../utils/api";
 
 export const useSellerAddressKyc = () => {
   const [loading, setLoading] = useState(false);
